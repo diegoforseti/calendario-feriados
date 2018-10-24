7f361d43528cbeff0c489f742b6d8664a34b00e8
Diego Saouda <diego.saouda@forseti.com.br>
2018-10-24 18:50:16 +0000

    Apply fixes from StyleCI

--- exemplos/index.php
+++ exemplos/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $feriados = new \Forseti\CalendarioFeriados\Feriado(['sp']);
 
@@ -14,7 +14,6 @@ var_dump($feriados->ehDiaUtil($date));
 
 echo "\nÉ feriado?: ";
 var_dump($feriados->ehFeriado($date));
-
 
 echo "\nPróximo dia útil: ";
 var_dump($feriados->proximoDiaUtil($date, 8));
